023f98e7f9175ac3c59715ca972d25d7755005d6
Janis Orlovs <janis.orlovs@gmail.com>
2018-07-30 17:28:18 +0300

    Cleaned packer configg

--- scripts/centos-7.5/cleanup.sh
+++ scripts/centos-7.5/cleanup.sh
@@ -7,6 +7,10 @@ if rpm -q --whatprovides kernel | grep -Fqv "$(uname -r)"; then
   rpm -q --whatprovides kernel | grep -Fv "$(uname -r)" | xargs sudo yum -y autoremove
 fi
 
+sudo rm -f /etc/ssh/ssh_host_*
+sudo rm -f /var/lib/NetworkManager/*
+sudo rm -rf /tmp/*
 sudo yum --enablerepo=epel clean all
+sudo rm -rf /var/cache/yum
 sudo yum history new
 sudo truncate -c -s 0 /var/log/yum.log
